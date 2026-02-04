53f79d3bf78b7313af1892567362ea064d3759ea
w-abdullahamir <w-abdullahamir>
2026-02-04 21:38:34 +0500

    app.json modified. Removed hard-coded stuff from android/* to prevent againt 'prebuild --clean'

---
+++ withAndroidSplits.js
@@ -0,0 +1,23 @@
+const { withAppBuildGradle } = require("@expo/config-plugins");
+
+module.exports = function withAndroidSplits(config) {
+	return withAppBuildGradle(config, (config) => {
+		if (config.modResults.language === "groovy") {
+			const splitsBlock = `
+    splits {
+        abi {
+            enable true
+            reset()
+            include "arm64-v8a", "x86_64"
+            universalApk false
+        }
+    }`;
+			// Injects the splits block into the android {} section of build.gradle
+			config.modResults.contents = config.modResults.contents.replace(
+				/android\s?{/,
+				`android {${splitsBlock}`
+			);
+		}
+		return config;
+	});
+};
